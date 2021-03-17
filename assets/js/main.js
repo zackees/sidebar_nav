b887ede16bb88250a6e15b6c9762892a9b79b264
zackees <z@zackees.com>
2021-03-17 14:20:56 -0700

    prune

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,18 +51,6 @@
 			var $t = $(this),
 				$bg = $('<div class="bg"></div>').appendTo($t),
 				on, off;
-
-			on = function () {
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
-
-				$window
-					.on('scroll._parallax', function () {
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
-					});
-			};
 			off = function () {
 				$bg
 					.addClass('fixed')
