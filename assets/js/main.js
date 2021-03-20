be6de17b93fe918da7a487c3c142960e53513520
zackees <z@zackees.com>
2021-03-19 17:49:48 -0700

    trim - remove transition at intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,11 +34,13 @@
 	};
 
 	// Play initial animations on page load.
+	/*
 	$window.on('load', function () {
 		window.setTimeout(function () {
 			$body.removeClass('is-preload');
 		}, 100);
 	});
+	*/
 
 	// Scrolly.
 	$('.scrolly').scrolly();
