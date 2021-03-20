2e9d41d9937ac942304241a2647661c5ebf50c03
zackees <z@zackees.com>
2021-03-19 17:43:23 -0700

    remove breakpoints dependency

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,18 +14,7 @@
 		$main = $('#main'),
 		$navPanelToggle, $navPanel, $navPanelInner;
 
-	// Breakpoints.
-	breakpoints({
-		default: ['1681px', null],
-		xlarge: ['1281px', '1680px'],
-		large: ['981px', '1280px'],
-		medium: ['737px', '980px'],
-		small: ['481px', '736px'],
-		xsmall: ['361px', '480px'],
-		xxsmall: [null, '360px']
-	});
-
-	/**
+    /**
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
 	 */
